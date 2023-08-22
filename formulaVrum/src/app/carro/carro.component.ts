45a9443f335d96124319bf055111eb543d3803ca
deborah-mattge <elomattge@gmail.com>
2023-08-22 16:56:49 -0300

    oi

--- formulaVrum/src/app/carro/carro.component.ts
+++ formulaVrum/src/app/carro/carro.component.ts
@@ -12,6 +12,7 @@ interface Carro{
   cambio: string
   consumoUrbano: string
   consumoRodoviario: string
+  imagem: string
 }
 @Component({
   selector: 'app-carro',
@@ -33,6 +34,7 @@ export class CarroComponent implements OnInit {
       cambio: 'Manual',
       consumoUrbano: '9,1 km/L (A) 13,1 km/L (G)',
       consumoRodoviario: '11,1 km/L (A) 16,1 km/L (G)'
+      ,imagem: '/assets/chevrolet-onix-joy.jpg'
     },
     {
       nome: ' Cruze LT 1.4 Turbo',
@@ -47,6 +49,8 @@ export class CarroComponent implements OnInit {
       cambio: 'Manual',
       consumoUrbano: '7,1 km/L (A), 10,5 km/L (G)',
       consumoRodoviario: '9,7 km/L(A), 13,9 km/L (G)'
+      ,imagem: '/assets/Cruze-Premier.jpg'
+
     },
     {
       nome: ' Tracker AT 1.0',
@@ -61,6 +65,40 @@ export class CarroComponent implements OnInit {
       cambio: 'Automático',
       consumoUrbano: '7,8 km/L (A) 11,2 km/L (G)',
       consumoRodoviario: '9,6 km/L (A) 13,6 km/L (G)'
+      ,imagem: '/assets/chevrolet-tracker.jpg'
+
+    },
+    {
+      nome: ' Tracker AT 1.0',
+      ano: 2023,
+      marca: 'Chevrolet',
+      preco: 113267,
+      combustivel: 'Flex',
+      ipva: 4531,
+      categoria: 'SUV',
+      lugares: 5,
+      tracao: 'Dianteira',
+      cambio: 'Automático',
+      consumoUrbano: '7,8 km/L (A) 11,2 km/L (G)',
+      consumoRodoviario: '9,6 km/L (A) 13,6 km/L (G)'
+      ,imagem: '/assets/chevrolet-tracker.jpg'
+
+    },
+    {
+      nome: ' Tracker AT 1.0',
+      ano: 2023,
+      marca: 'Chevrolet',
+      preco: 113267,
+      combustivel: 'Flex',
+      ipva: 4531,
+      categoria: 'SUV',
+      lugares: 5,
+      tracao: 'Dianteira',
+      cambio: 'Automático',
+      consumoUrbano: '7,8 km/L (A) 11,2 km/L (G)',
+      consumoRodoviario: '9,6 km/L (A) 13,6 km/L (G)'
+      ,imagem: '/assets/chevrolet-tracker.jpg'
+
     }
   ]
       constructor() { }
@@ -89,6 +127,8 @@ export class CarroComponent implements OnInit {
       cambio: 'Automático de 6 marchas',
       consumoUrbano: '7,8 km/L (A) 11,2 km/L (G)',
       consumoRodoviario: '9,6 km/L (A) 13,6 km/L (G)'
+      ,imagem: 'src/assets/chevrolet-onix-joy.jpg'
+
   
     };
   // Adiciona o novo carro à lista
