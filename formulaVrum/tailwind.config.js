942b6e9ea435220c60052d6968f16fb1b78066c2
GabrielLukaA <gabriel_luka-amaral@estudante.sesisenai.org.br>
2023-08-23 13:39:40 -0300

    mobile faltando modal

--- formulaVrum/tailwind.config.js
+++ formulaVrum/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
   },
   plugins: [],
 }
