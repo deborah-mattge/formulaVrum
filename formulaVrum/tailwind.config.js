6e616468043bf68413d46fa9eace08c80adb6d5f
GabrielLukaA <gabriel_luka-amaral@estudante.sesisenai.org.br>
2023-08-23 21:49:42 -0300

    home finished, mobile my first car complete.

--- formulaVrum/tailwind.config.js
+++ formulaVrum/tailwind.config.js
@@ -4,12 +4,17 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      s650: "650px",
+      md: "768px",
+      s900: "900px",
+      lg: "976px",
+      s1100: "1100px",
+      s1300: "1300px",
+      s1400: "1400px",
+      xl: "1440px",
+      s1900: "1900px",
     },
   },
   plugins: [],
-}
-
+};
